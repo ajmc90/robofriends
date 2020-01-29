36a1c1db9030be1d08967e2fc15654a0c7bda34e
Andres <andres.mejias@globant.com>
2020-01-29 11:28:02 -0800

    Robo friends init

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import './Card.css'
+
+const Card = ({id, name, email }) => {
+  // const {id, name, email } = props;
+  return (
+    <Fragment>
+      <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 w-20">
+        <img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
+        <div>
+          <h2 title={name}>{name}</h2>
+          <p>{email}</p>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howel',
+    username: 'Antonneette',
+    email: 'Shanna@april.biz'
+  },
+  {
+    id: 3,
+    name: 'Andres Mejias',
+    username: 'Andres',
+    email: 'andres@april.biz'
+  }
+]
