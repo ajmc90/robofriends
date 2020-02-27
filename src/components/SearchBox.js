88d0b79baa49fb92c8d49462eb91232b308abd32
Andres <andres.mejias@globant.com>
2020-02-27 06:44:16 -0800

    Adding the aria label to the search input and add the font-display swap

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,8 @@ const SearchBox = ({searchfield, searchChange}) => {
   return (
     <div className="pa2">
       <input 
-      className="pa3 ba b--green bg-lightest-blue"
+        aria-label="Search Robots"
+        className="pa3 ba b--green bg-lightest-blue"
         type="search" 
         placeholder='search robots' 
         onChange={searchChange}
